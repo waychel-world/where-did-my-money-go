2e59f4d36f041cd0185009f2d4836d14c58a73df
waychel-l <160815005+waychel-l@users.noreply.github.com>
2024-10-23 13:22:07 +0800

    Fix parsing of pdf to create clean dataframe
    Use concat instead of append to generate transactions in individual rows instead of list of entries
    Add column headers to ensure proper alignment during concatenation
    Add pandas option = headers:none to solve issue of first row of each temp_df being overwritten by column name
    Filter away unwanted rows, containing reference number or foreign currency

--- script.py
+++ script.py
@@ -8,28 +8,33 @@ file_path = r"/Users/roois/Downloads/eStatement.pdf"
 reader = pypdf.PdfReader(open(file_path, mode='rb' ))
 n = len(reader.pages) 
 
-df = []
+df = pd.DataFrame()
+
 for page in [str(i+1) for i in range(n)]:
     if page == "1":
-            df.append(read_pdf(file_path, area=(400,12.75,790.5,560), pages=page))
-    elif int(page) > 6:
+            temp_df = pd.concat(read_pdf(file_path, area=(400,12.75,790.5,560), columns = [94, 140, 452], pages=page, guess=False, pandas_options={'header': None}))
+            temp_df.columns = ['Trans Date', 'Post Date', 'Description', 'Amount (SGD)']
+            df = pd.concat([df, temp_df], axis=0, ignore_index=True)
+    elif int(page) > (n-2):
             continue
     else:
-            df.append(read_pdf(file_path, area=(170,12.75,790.5,560), pages=page))
+            temp_df = pd.concat(read_pdf(file_path, area=(170,12.75,790.5,560), columns = [94, 140, 452], pages=page, guess=False, pandas_options={'header': None}))
+            temp_df.columns = ['Trans Date', 'Post Date', 'Description', 'Amount (SGD)']
+            df = pd.concat([df, temp_df], axis=0, ignore_index=True)
 
 
-data_frame = pd.DataFrame(df)
+# Filter the DataFrame to remove the unwanted rows
+unwanted_strings = ['Ref No.', 'SUB TOTAL', 'TOTAL BALANCE', 'End of Transaction Details', 'MYR', 'USD', 'TWD', 'INR']
+condition = df['Description'].str.contains('|'.join(unwanted_strings))
+df_cleaned = df[~condition].reset_index(drop=True)
 
 # Save DataFrame to CSV
 csv_output_path = 'transactions.csv'
-data_frame.to_csv(csv_output_path, index=False)
+df_cleaned.to_csv(csv_output_path, index=False)
 
-
+print('success!') 
 
 #FIXES
-#transactions on the last page don't show 
-#if row contains NaN remove it 
 #tagging function
 #reformat for my personal finance excel sheet
 #duplicate and modify bot for the UOB bank statement 
-#sort deposits and withdrawals into different tables
